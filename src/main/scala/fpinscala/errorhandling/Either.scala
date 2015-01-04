a9a36f7dab34e29038c87e3612df8c9faaca01d6
Franz Granlund <franz.granlund@gmail.com>
2015-01-04 22:20:13 +0200

    p 63

---
+++ src/main/scala/fpinscala/errorhandling/Either.scala
@@ -0,0 +1,53 @@
+package fpinscala.errorhandling
+
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => Right(f(a))
+  }
+
+  def flatMap[EE >: E,B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => f(a)
+  }
+
+  def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(a) => Right(a)
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    this.flatMap(a => b.map(b1 => f(a, b1)))
+
+}
+
+case class Left[+E](value: E) extends Either[E, Nothing]
+case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] = {
+    if (xs.isEmpty)
+      Left("mean of empty list!")
+    else
+      Right(xs.sum / xs.length)
+  }
+
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] = {
+    try Right(x / y)
+    catch { case e: Exception => Left(e) }
+  }
+
+  def Try[A](a: => A): Either[Exception, A] = {
+    try Right(a)
+    catch { case e: Exception => Left(e) }
+  }
+
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    traverse(es)(e => e)
+
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    as match {
+      case Nil => Right(Nil)
+      case h :: t => f(h).map2(traverse(t)(f))((h, t) => h :: t)
+    }
+}
