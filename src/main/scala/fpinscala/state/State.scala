22f6850a1c8e6a1d6d8a528bcb5a506c5e70f99b
Franz Granlund <franz.granlund@gmail.com>
2015-01-18 15:32:52 +0200

    p 87

--- src/main/scala/fpinscala/state/State.scala
+++ src/main/scala/fpinscala/state/State.scala
@@ -66,4 +66,74 @@ object RNG {
     go(count, rng, List())
   }
 
+  type Rand[+A] = RNG => (A, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def nonNegativeEven: Rand[Int] =
+    map(nonNegativeInt)(i => i - i % 2)
+
+  def double_1: Rand[Double] =
+    map(nonNegativeInt)(i => i / (Int.MaxValue.toDouble + 1))
+
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    rng => {
+      val (a, r1) = ra(rng)
+      val (b, r2) = rb(r1)
+      (f(a, b), r2)
+    }
+  }
+
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] = {
+    map2(ra, rb)((_, _))
+  }
+
+  val randIntDouble: Rand[(Int, Double)] =
+    both(int, double_1)
+
+  val randDoubleInt: Rand[(Double, Int)] =
+    both(double_1, int)
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+  }
+
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = {
+    rng => {
+      val (a, r1) = f(rng)
+      val (b, r2) = g(a)(r1)
+      (b, r2)
+    }
+  }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n-1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
+  }
+
+  def map_1[A, B](s: Rand[A])(f: A => B): Rand[B] = {
+    flatMap(s){ a =>
+      unit(f(a))
+    }
+  }
+
+  def map_2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    flatMap(ra) { a =>
+      map_1(rb)(b => f(a, b))
+    }
+  }
+
+  def rollDie: Rand[Int] = map(nonNegativeLessThan(6))(_ + 1)
+
 }
